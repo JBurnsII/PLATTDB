e8d3a22278f0ecc693df3f462363fc4da6c0b3e4
Justin Burns II <jburns1@trinity.edu>
2023-12-01 11:52:56 -0600

    database connection

--- connection.js
+++ connection.js
@@ -1,11 +1,49 @@
-const Client = require('pg').Client;
+const {Client} = require('pg');
 
 const client = new Client({
-    host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com",
     user: "Advisr",
+    host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com",
     port: 5432,
     password: "Gotigers",
-    database: "PLATTDB"
+    database: "PLATTDB",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-module.exports = client;
+client.connect(function(err) {
+    if(err) throw err;
+    console.log("connected")
+});
+
+// const res = await client.query('SELECT * FROM judges')
+// console.log(res)
+
+// module.exports = client;
+console.log("before query")
+client.query("SELECT * FROM judges Where judges_id = 500", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+ 
+// const connectDb = async () => {
+//     try {
+//         const client = new Client({
+//             user: "Advisr",
+//             host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com",
+//             database: "PLATTDB",
+//             password: "Gotigers",
+//             port: 5432,
+//         })
+ 
+//         await client.connect()
+//         const res = await client.query('SELECT * FROM judges')
+//         console.log(res)
+//         await client.end()
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+ 
+// connectDb()
