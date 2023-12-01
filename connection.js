a49892ab4db495b487a4241788b25ebb3d60ded0
Justin Burns II <jburns1@trinity.edu>
2023-11-30 19:06:04 -0600

    l

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com/PLATTDB",
+    host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com/",
     user: "Advisr",
     port: 5432,
     password: "Gotigers",
