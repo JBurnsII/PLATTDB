96afd03618ec19e32355c44de2f8562e37eb78e5
Justin Burns II <jburns1@trinity.edu>
2023-12-01 11:56:05 -0600

    cleanup useless comments

--- connection.js
+++ connection.js
@@ -16,34 +16,8 @@ client.connect(function(err) {
     console.log("connected")
 });
 
-// const res = await client.query('SELECT * FROM judges')
-// console.log(res)
-
-// module.exports = client;
 console.log("before query")
 client.query("SELECT * FROM judges Where judges_id = 500", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
-
- 
-// const connectDb = async () => {
-//     try {
-//         const client = new Client({
-//             user: "Advisr",
-//             host: "tigradvisr.cmhpzn7cmtwl.us-east-1.rds.amazonaws.com",
-//             database: "PLATTDB",
-//             password: "Gotigers",
-//             port: 5432,
-//         })
- 
-//         await client.connect()
-//         const res = await client.query('SELECT * FROM judges')
-//         console.log(res)
-//         await client.end()
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
- 
-// connectDb()
